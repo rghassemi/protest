faebd5879985bb8a375db87f2ac973388a40dd70
rghassemi <rezajghassemi@yahoo.com>
2017-04-15 16:57:28 +0000

    adding example

--- display.py
+++ display.py
@@ -26,7 +26,7 @@ def get_ascii_pixels(character, rotate=None):
     return temp
 
 
-class Cocktapus(object):
+class Hardware(object):
     def __init__(self):
         self.options = RGBMatrixOptions()
         self.options.hardware_mapping = 'adafruit-hat'
@@ -167,7 +167,7 @@ class LineCursor(object):
 
 class ThreeLineDisplay(object):
     def __init__(self):
-        self.hardware = Cocktapus()
+        self.hardware = Hardware()
         self.canvas = self.hardware.canvas
         self.forward_lines = []
         self.rotated_lines = []
@@ -204,28 +204,7 @@ class ThreeLineDisplay(object):
     def scroll_line(self, line_number):
         self.forward_lines[line_number].scroll()
         self.rotated_lines[line_number].scroll()
-if __name__ == "__main__":
-    import time
-    display = ThreeLineDisplay()
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
 
-    first_line = ['Life', 'Music', 'Friends', 'Love', 'BBQ']
-    second_line = ['Before', 'Instead of', '>']
-    third_line = ['Politics', 'Parties', 'Greed', 'Apathy']
-
-    display.draw_the_line(line_number=0, queue=first_line, color=RED)
-    display.draw_the_line(line_number=1, queue=second_line, start_column=15, color=GREEN)
-    display.draw_the_line(line_number=2, queue=third_line, start_column=15,  color=BLUE)
-    display.update()
-
-    while True:
-        time.sleep(0.05)
-        display.scroll_line(0)
-        #display.scroll_line(1)
-        #display.scroll_line(2)
-        #display.update()
 
 
 
---
+++ example.py
@@ -0,0 +1,23 @@
+from display import ThreeLineDisplay
+import time
+
+if __name__ == "__main__":
+    display = ThreeLineDisplay()
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+
+    first_line = ['Life', 'Music', 'Friends', 'Love', 'BBQ']
+    second_line = ['Before', 'Instead of', '>']
+    third_line = ['Politics', 'Parties', 'Greed', 'Apathy']
+
+    display.draw_the_line(line_number=0, queue=first_line, color=RED)
+    display.draw_the_line(line_number=1, queue=second_line, start_column=15, color=GREEN)
+    display.draw_the_line(line_number=2, queue=third_line, start_column=15,  color=BLUE)
+    display.update()
+
+    while True:
+        time.sleep(0.05)
+        display.scroll_line(0)
+        display.scroll_line(1)
+        display.scroll_line(2)
